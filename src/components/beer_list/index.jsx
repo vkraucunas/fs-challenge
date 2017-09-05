ecc9396f9840da86d2e1427bbe152662a81634a9
Valerie Kraucunas <valeriekraucunas@gmail.com>
2017-09-05 15:14:43 -0600

    beer list component created. updated title

---
+++ src/components/beer_list/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import s from './index.styl'
+import withStyles from 'isomorphic-style-loader/lib/withStyles'
+
+
+class BeerList extends React.Component {
+  render() {
+    return (
+      <div className={s.yellow}>
+        <h2>HELLO</h2>
+      </div>
+    )
+  }
+}
+
+export default BeerList

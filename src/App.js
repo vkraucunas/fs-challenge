6e8382060b6a329efcd568e476a5257078700e6b
Valerie Kraucunas <valeriekraucunas@gmail.com>
2017-09-05 14:18:09 -0600

    jsx to go with last commit of case change for classNames

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import BottleCap from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="app">
+        <div className="app-header">
+          <img src={BottleCap} className="logo" alt="Bottlecap logo next to name beer.status()" />
           <h2>beer.status()</h2>
         </div>
-        <p className="App-intro">
+        <p className="app-intro">
           Let's make a thing.
         </p>
       </div>

b10642b68a9974dd88345fb1e6561f13473bffde
Valerie Kraucunas <valeriekraucunas@gmail.com>
2017-09-05 14:48:27 -0600

    added css modules

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -180,6 +180,7 @@ module.exports = {
                       loader: require.resolve('css-loader'),
                       options: {
                         importLoaders: 1,
+                        modules: true,
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
                       },

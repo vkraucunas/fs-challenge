cd253489fcf5ec4a1488a8faffb25aa42b648ee4
Valerie Kraucunas <valeriekraucunas@gmail.com>
2017-09-05 21:12:27 -0600

    renamed style sheet. svg added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import BottleCap from './logo.svg';
 import BeerList from './components/beer_list'
-import s from './App.css';
+import s from './index.styl';
 
 class App extends Component {
   render() {
     return (
       <div className={s.app}>
-        <div className={s.appHeader}>
+        <div className={s.header}>
           <img src={BottleCap} className={s.logo} alt="Bottlecap logo next to name beer.status()" />
           <h2>beer.status()</h2>
         </div>
-        <p className={s.appIntro}>
+        <p className={s.intro}>
           Let's make a thing.
         </p>
         <BeerList />

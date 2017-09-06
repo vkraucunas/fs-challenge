d0d82bd68fd3e3af021f735fcefd16f2c60aa3d2
Valerie Kraucunas <valeriekraucunas@gmail.com>
2017-09-05 21:14:59 -0600

    components file structure set up

--- src/components/beer_list/index.jsx
+++ src/components/beer_list/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import s from './index.styl'
-import withStyles from 'isomorphic-style-loader/lib/withStyles'
 
 
 class BeerList extends React.Component {
   render() {
     return (
-      <div className={s.yellow}>
-        <h2>HELLO</h2>
+      <div className={s.beerList}>
+        <h2>Something</h2>
       </div>
     )
   }
